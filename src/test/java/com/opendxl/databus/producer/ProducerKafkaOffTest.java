e35626685852e0246e0dc5a199b0c6a7a7f2b7c6
rliberal <rodrigo_liberal@mcafee.com>
2019-08-26 17:59:24 -0300

    Added producer tests

--- src/test/java/com/opendxl/databus/producer/ProducerKafkaOffTest.java
+++ src/test/java/com/opendxl/databus/producer/ProducerKafkaOffTest.java
@@ -35,4 +35,6 @@ public class ProducerKafkaOffTest {
             Assert.fail(e.getMessage());
         }
     }
+
+
 }
--- src/test/java/com/opendxl/databus/producer/ProducerKafkaOnTest.java
+++ src/test/java/com/opendxl/databus/producer/ProducerKafkaOnTest.java
@@ -8,15 +8,14 @@ import broker.ClusterHelper;
 import com.opendxl.databus.util.Constants;
 import com.opendxl.databus.util.ProducerHelper;
 import com.opendxl.databus.util.TestCallback;
-import junit.framework.AssertionFailedError;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class ProducerKafkaOnTest {
 
@@ -55,4 +54,99 @@ public class ProducerKafkaOnTest {
             Assert.fail(e.getMessage());
         }
     }
+
+    @Test
+    public void produceMessageSuccessDefaultCallbackTest() {
+        try {
+            final ProducerHelper producerHelper = new ProducerHelper();
+            final Producer<byte[]> producer = producerHelper.getProducer();
+            final String producerTopic = "topic1test";
+            final ProducerRecord<byte[]> producerRecord = producerHelper.getRecord(producerTopic,
+                    String.valueOf(System.currentTimeMillis()));
+
+            // Send the record
+            producer.send(producerRecord);
+
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void getPartitionsForTest() {
+        try {
+            final ProducerHelper producerHelper = new ProducerHelper();
+            final Producer<byte[]> producer = producerHelper.getProducer();
+            final String producerTopic = "topic1test";
+            final ProducerRecord<byte[]> producerRecord = producerHelper.getRecord(producerTopic,
+                    String.valueOf(System.currentTimeMillis()));
+
+            // Send the record
+            producer.send(producerRecord);
+
+            Assert.assertNotNull(producer.partitionsFor(producerTopic));
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void closeSentWithTimeOutTest() {
+        try {
+            final long timeout = 2;
+            final ProducerHelper producerHelper = new ProducerHelper();
+            final Producer<byte[]> producer = producerHelper.getProducer();
+            final String producerTopic = "topic1test";
+            final ProducerRecord<byte[]> producerRecord = producerHelper.getRecord(producerTopic,
+                    String.valueOf(System.currentTimeMillis()));
+
+            // Send the record
+            producer.send(producerRecord);
+
+            producer.close(timeout, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void producerFlushSuccessTest(){
+        try {
+        final ProducerHelper producerHelper = new ProducerHelper();
+        final Producer<byte[]> producer = producerHelper.getProducer();
+
+        producer.flush();
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void producerGetMetrics(){
+        try {
+            final ProducerHelper producerHelper = new ProducerHelper();
+            final Producer<byte[]> producer = producerHelper.getProducer();
+            String producerTopic = "topic1";
+
+
+            Assert.assertNotNull(producer.recordBatchSizeAvgMetric());
+            Assert.assertNotNull(producer.recordBatchSizeMaxMetric());
+            Assert.assertNotNull(producer.recordErrorRateMetric());
+            Assert.assertNotNull(producer.recordErrorTotalMetric());
+            Assert.assertNotNull(producer.recordSendRateMetric());
+            Assert.assertNotNull(producer.recordSendTotalMetric());
+            Assert.assertNotNull(producer.recordSizeAvgMetric());
+            Assert.assertNotNull(producer.recordSizeMaxMetric());
+            Assert.assertNotNull(producer.recordSendRatePerTopicMetric(producerTopic));
+            Assert.assertNotNull(producer.recordSendTotalPerTopicMetric(producerTopic));
+            Assert.assertNotNull(producer.recordByteRatePerTopicMetric(producerTopic));
+            Assert.assertNotNull(producer.recordByteTotalPerTopicMetric(producerTopic));
+            Assert.assertNotNull(producer.recordErrorRatePerTopicMetric(producerTopic));
+            Assert.assertNotNull(producer.recordErrorTotalPerTopicMetric(producerTopic));
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+
 }
