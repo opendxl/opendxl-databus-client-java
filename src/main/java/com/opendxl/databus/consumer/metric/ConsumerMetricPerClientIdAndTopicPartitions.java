4f1b04fbc4e84a72beed6d87bab44c3c8b77e522
rliberal <rodrigo.inf.liberal@gmail.com>
2019-09-26 14:47:31 -0300

    Added javadoc to consumer metrics package

--- src/main/java/com/opendxl/databus/consumer/metric/ConsumerMetricPerClientIdAndTopicPartitions.java
+++ src/main/java/com/opendxl/databus/consumer/metric/ConsumerMetricPerClientIdAndTopicPartitions.java
@@ -10,9 +10,22 @@ import java.util.Map;
 
 public class ConsumerMetricPerClientIdAndTopicPartitions {
 
+    /**
+     * The client id.
+     */
     private final String clientId;
+
+    /**
+     * A map object with contains topic partition as key and a consumer metric as value.
+     * This map stores the consumer metric data for each topic partition of the consumer.
+     */
     private final Map<TopicPartition, ConsumerMetric> topicPartitionsMetrics;
 
+    /**
+     * ConsumerMetricPerClientIdAndTopicPartitions constructor.
+     * @param clientId An associated clientId.
+     * @param topicPartitionsMetrics An associated map with topic-partition as key an consumerMetric as value.
+     */
     public
     ConsumerMetricPerClientIdAndTopicPartitions(final String clientId,
                                                 final Map<TopicPartition, ConsumerMetric> topicPartitionsMetrics) {
@@ -20,10 +33,18 @@ public class ConsumerMetricPerClientIdAndTopicPartitions {
         this.topicPartitionsMetrics = topicPartitionsMetrics;
     }
 
+    /**
+     * Returns the map of topic partitions with an associated consumer metric value.
+     * @return A map object of topic-partition as key and consumer metric as value.
+     */
     public Map<TopicPartition, ConsumerMetric> getTopicPartitionsMetrics() {
         return topicPartitionsMetrics;
     }
 
+    /**
+     * Returns the client id.
+     * @return The client id.
+     */
     public String getClientId() {
         return clientId;
     }
